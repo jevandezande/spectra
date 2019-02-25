4fea9b548ec00579c295cd72fa26f68b8dd658de
Jonathon Vandezande <jevandezande@gmail.com>
2019-02-25 11:01:52 -0500

    Use area for plotting peak progress.

--- spectra/progress.py
+++ spectra/progress.py
@@ -5,43 +5,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-from .tools import read_csvs
+from .tools import read_csvs, y_at_x, integrate
 
 
-def plot_progress(xs, ys, times, x_point, fit=None, savefig=False, colors=None):
+def plot_progress(xs, ys, times, x_points, x_units='hours', fit=None, savefig=False, colors=None):
     """
     Plot the change of the height of a point across time
-    :param inps: list of input files
+    :param xs, ys: 2D arrays of x values and y_values at `times`
+    :param times: time at which curves were taken
+    :param x_points: range of xs to integrate over
     :param fit: plot a linear fit
     :param savefig: save the figure to the specified file name
     """
-    # Find the height at the specified point
-    heights = []
-    for x_vals, y_vals in zip(xs, ys):
-        for x, y in zip(x_vals, y_vals):
-            if x >= x_point:
-                heights.append(y)
-                break
+    fig, ax = plt.subplots()
 
-    heights = heights[1:]
-    plt.plot(times, heights)
-    plt.gca().set_xlabel('Time (min)')
-    plt.gca().set_ylabel(f'Absorbance at {x_point} cm$^{{-1}}$')
+    # Find the height at the specified point
+    #heights = [y_at_x(x_point, x_vals, y_vals) for x_vals, y_vals in zip(xs, ys)]
+    areas = [integrate(x_vals, y_vals, x_points) for x_vals, y_vals in zip(xs, ys)]
+    # Remove negative numbers
+    areas = [a if a > 0 else 0 for a in areas]
+
+    ax.plot(times, areas)
+    ax.set_xlabel(f'Time ({x_units})')
+    ax.set_ylabel(f'Absorbance peak area at (${x_points[0]}-{x_points[1]}$ cm$^{{-1}}$)')
 
     if colors:
-        plt.scatter(times, heights, color=colors, zorder=100)
+        ax.scatter(times, areas, color=colors, zorder=100)
 
     if fit:
-        slope, intercept, r_value, p_value, std_err = stats.linregress(times, heights)
+        slope, intercept, r_value, p_value, std_err = stats.linregress(times, areas)
         function = np.poly1d((slope, intercept))
         ends = (times[0], times[-1])
         label = f"     Fit -- R$^2$ = {r_value**2:3.2f}\n$y$ = {slope:5.5f}$X$ + {intercept:5.2f}"
-        plt.plot(ends, function(ends), label=label)
+        ax.plot(ends, function(ends), label=label)
 
-    plt.gca().set_ylim(bottom=0)
-    plt.legend()
-    plt.suptitle(f'Peak Progress ({x_point} cm$^{{-1}}$)')
+    ax.set_ylim(bottom=0)
+    #ax.legend()
+    fig.suptitle(f'Peak Progress (${x_points[0]}-{x_points[1]}$ cm$^{{-1}}$)')
 
     if savefig:
-        plt.savefig(savefig)
-    plt.show()
+        fig.savefig(savefig)
+
+    return fig, ax

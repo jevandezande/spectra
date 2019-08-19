d5a9ab4401f3e04e2318f2c225c26086ab0f1a7f
Jonathon Vandezande <jevandezande@gmail.com>
2019-08-19 14:52:02 -0400

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='spectra',
+    version='',
+    packages=[],
+    url='',
+    license='MIT',
+    author='Jonathon Vandezande',
+    author_email='jevandezande@gmail.com',
+    description='A simple tool for plotting spectra',
+    install_requires=['matplotlib', 'numpy', 'pytest', 'scipy'],
+)

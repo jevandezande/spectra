3bdb67078128f096a0f237f72f8ff22edc16e508
Jonathon Vandezande <jevandezande@gmail.com>
2019-11-15 16:30:40 -0500

    Adds Travis support!

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='spectra',
-    version='',
-    packages=[],
+    version='0.1',
+    packages=['spectra'],
     url='',
     license='MIT',
     author='Jonathon Vandezande',
     author_email='jevandezande@gmail.com',
     description='A simple tool for plotting spectra',
-    install_requires=['matplotlib', 'numpy', 'pytest', 'scipy'],
+    install_requires=['matplotlib', 'natsort', 'numpy', 'pytest', 'scipy'],
 )

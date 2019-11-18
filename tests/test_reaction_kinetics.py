6c893e2ad5e9c0e9964eccde409a57a8b0f49f64
Jonathon Vandezande <jevandezande@gmail.com>
2019-11-18 14:51:52 -0500

    Adds new tests for reaction_kinetics.py

--- tests/test_reaction_kinetics.py
+++ tests/test_reaction_kinetics.py
@@ -2,8 +2,9 @@ import sys
 
 sys.path.insert(0, '..')
 
-from spectra.reaction_kinetics import plot_reaction_kinetics
+import pytest
 from pytest import raises
+from spectra.reaction_kinetics import plot_reaction_kinetics
 
 
 def setup():
@@ -15,8 +16,21 @@ def teardown():
 
 
 def test_plot_reaction_kinetics():
-    fig, axes = plot_reaction_kinetics([], [], '')
+    fig, axes = plot_reaction_kinetics([], '', verbose=True)
     assert len(axes) == 1  # combo plot
 
     with raises(ValueError):
-        plot_reaction_kinetics([], [], '', colors=['red', 'blue'])
+        plot_reaction_kinetics([], '', colors=['red', 'blue'])
+
+
+@pytest.mark.slow
+def test_plot_reaction_kinetics_full():
+    fig, axes = plot_reaction_kinetics([], '', verbose=True)
+    assert len(axes) == 1  # combo plot
+
+    with raises(ValueError):
+        plot_reaction_kinetics([], '', colors=['red', 'blue'])
+
+    catalysts = ['0.01% T12', '0.03% T12', '0.10% T12', '0.30% T12', '1.00% T12']
+    reactions = [f'1-butanol + N 3400/{catalyst}' for catalyst in catalysts]
+    fig, axes = plot_reaction_kinetics(reactions, 'files')

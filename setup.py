3378d37a58d7cf34a4c6f0f59b16953a3fb107c6
Jonathon Vandezande <jevandezande@gmail.com>
2019-12-05 10:39:11 -0500

    Updates setup.py and bumps version.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='spectra',
-    version='0.1',
+    version='0.2',
+    description='Utility for analyzing and plotting 1D spectra.',
+    long_description=readme(),
     packages=['spectra'],
     url='',
     license='MIT',
     author='Jonathon Vandezande',
     author_email='jevandezande@gmail.com',
-    description='A simple tool for plotting spectra',
     install_requires=['matplotlib', 'natsort', 'numpy', 'pytest', 'scipy'],
+    tests_require=['pytest'],
 )

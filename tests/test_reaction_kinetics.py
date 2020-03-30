dab4f8b71a5a3001584c2255d383ac33f00532d2
Jonathon Vandezande <jevandezande@gmail.com>
2020-03-30 14:06:45 -0400

    Adds extra tests for plot_reaction_kinetics().

--- tests/test_reaction_kinetics.py
+++ tests/test_reaction_kinetics.py
@@ -16,29 +16,30 @@ def teardown():
     pass
 
 
-def test_plot_reaction_kinetics():
-    fig, axes = plot_reaction_kinetics([], '', verbose=True)
+def test_plot_reaction_kinetics(tmp_path):
+    fig, axes = plot_reaction_kinetics([], '', verbose=True, title='Hello World')
     assert len(axes) == 1  # combo plot
 
-    fig, axes = plot_reaction_kinetics([], '', verbose=True, rounds=[3])
+    fig, axes = plot_reaction_kinetics([], '', verbose=True, rounds=[3], savefig=f'{tmp_path}/a.svg')
 
     with raises(ValueError):
         plot_reaction_kinetics([], '', colors=['red', 'blue'])
 
 
-def test_plot_reaction_kinetics_full():
+def test_plot_reaction_kinetics_full(tmp_path):
     fig, axes = plot_reaction_kinetics([], '', verbose=True)
     assert len(axes) == 1  # combo plot
 
     with raises(ValueError):
         plot_reaction_kinetics([], '', colors=['red', 'blue'])
 
-    catalysts = ['0.01% T12']
+    catalysts = ['1.00% T12']
     reactions = [f'1-butanol + N 3400/{catalyst}' for catalyst in catalysts]
-    fig, axes = plot_reaction_kinetics(reactions, 'tests/files/', verbose=True)
+    fig, axes = plot_reaction_kinetics(reactions, 'tests/files/', verbose=True,
+                                       spectra_norms=('max', 1), spectra_smooth=True)
     assert axes.shape == (2, 2)
 
-    catalysts = ['0.01% T12', '0.03% T12']#, '0.10% T12', '0.30% T12', '1.00% T12']
+    catalysts = ['1.00% T12', '0.10% T12'] #, '0.30% T12', '1.00% T12']
     reactions = [f'1-butanol + N 3400/{catalyst}' for catalyst in catalysts]
-    fig, axes = plot_reaction_kinetics(reactions, 'tests/files/', verbose=True, rounds=[5])
+    fig, axes = plot_reaction_kinetics(reactions, 'tests/files/', verbose=True, rounds=[5], savefig=f'{tmp_path}/a.svg')
     assert axes.shape == (3, 2)

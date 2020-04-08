698bbf48c726fd2dbc2c0985ebf6eb2e261755f2
Jonathon Vandezande <jevandezande@gmail.com>
2020-04-08 09:22:49 -0400

    Updates sample IR plot made by zsh.

--- samples/IR/ir.zsh
+++ samples/IR/ir.zsh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 PYTHONPATH=../../:$PYTHONPATH
 PATH=../../bin:$PATH
 
-plot_spectra -i data/{HDI,methanol}.csv -s plots/ir_zsh.svg -n 'HDI' 'Methanol' --title "IR Spectra" -p
+plot_spectra -i data/{HDI,methanol}.csv -s plots/ir_zsh.svg -n 'HDI' 'Methanol' --title 'IR Spectra' -p

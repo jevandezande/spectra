91d51998964dad7c522c308c15f95da4576f11c2
Jonathon Vandezande <jevandezande@gmail.com>
2022-04-20 15:09:42 -0400

    Removes extraneous data from spectra package.

--- spectra/__init__.py
+++ spectra/__init__.py
@@ -2,8 +2,4 @@
 from .conv_spectrum import ConvSpectrum
 from .sticks_spectrum import SticksSpectrum
 
-__author__ = """Jonathon Vandezande"""
-__email__ = "jevandezande@gmail.com"
-__version__ = "0.4.0"
-
 __all__ = ["ConvSpectrum", "SticksSpectrum"]
